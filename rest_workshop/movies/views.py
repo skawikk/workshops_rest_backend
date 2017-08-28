f4b81f97e95d1295b8580468ae750c1930996a62
skawikk <skawikk>
2017-08-28 15:05:19 +0200

    exercise 10, that's all;)

--- rest_workshop/movies/views.py
+++ rest_workshop/movies/views.py
@@ -6,8 +6,9 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from movies.models import Movies
-from movies.serializers import MoviesSerializer
+import movies
+from movies.models import Movies, Person
+from movies.serializers import MoviesSerializer, PersonSerializer
 
 
 class MoviesView(APIView):
@@ -50,6 +51,41 @@ class MovieView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class PersonsView(APIView):
+    def get(self, request):
+        persons = Person.objects.all()
+        serializer = PersonSerializer(persons, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PersonSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class PersonView(APIView):
+    def get_object(self, pk):
+        try:
+            return Person.objects.get(pk=pk)
+        except Person.DoesNotExist:
+            raise Http404
 
+    def get(self, request, pk):
+        person = self.get_object(pk)
+        serializer = PersonSerializer(person)
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        person = self.get_object(pk)
+        person.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def put(self, request, pk):
+        person = self.get_object(pk)
+        serializer = PersonSerializer(person, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
