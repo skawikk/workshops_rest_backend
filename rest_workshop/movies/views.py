039d450075482f67034f81bef41ef994e7a9577f
skawikk <skawikk>
2017-08-28 14:17:47 +0200

    exercise 9

--- rest_workshop/movies/views.py
+++ rest_workshop/movies/views.py
@@ -16,6 +16,13 @@ class MoviesView(APIView):
         serializer = MoviesSerializer(movies, many=True)
         return Response(serializer.data)
 
+    def post(self, request):
+        serializer = MoviesSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class MovieView(APIView):
     def get_object(self, pk):
@@ -41,3 +48,8 @@ class MovieView(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
