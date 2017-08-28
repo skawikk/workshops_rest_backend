e1f21a38a2beedf2862d620d534aa0dc110f857d
skawikk <skawikk>
2017-08-28 13:49:02 +0200

    exercise 5

--- rest_workshop/movies/views.py
+++ rest_workshop/movies/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from movies.models import Movies
+from movies.serializers import MoviesSerializer
+
+
+class MoviesView(APIView):
+    def get(self, request):
+        movies = Movies.objects.all()
+        serializer = MoviesSerializer(movies, many=True)
+        return Response(serializer.data)
--- rest_workshop/rest_workshop/settings.py
+++ rest_workshop/rest_workshop/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'g3a@w))74oz(iibhmn7k)p@6^cimv$0q7n$71s3s-#=^%mffbd'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'rest_workshop.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -88,9 +85,6 @@ DATABASES = {
         },
     }
 }
-
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -110,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -123,7 +116,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
